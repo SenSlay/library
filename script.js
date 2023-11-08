024bdc53a3d26e9fb33d29898d330fe3c6606417
Irvan Castro <castroirvan@gmail.com>
2023-11-08 15:14:20 +0800

    -Create the template with initial styling for the  books -Create the constructor function for Book

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 // Array for books
 const library = [];
 
-
-
-function Book() {
-    
+// Constructor function for book
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = Boolean(read);
 };
 
+const sampleBook = new Book("Atomic Habits", "James Clear", 310, true);
+
+console.log(sampleBook);
 
 
-function addBookToLibrary() {
+function addBookToLibrary(book) {
+    library.push(book);
+};
 
+addBookToLibrary(sampleBook);
+
+console.log(library);
+
+
+function displayBook() {
+    
 };
