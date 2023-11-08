6821dfb4b2f29424afb80adc1316972c533ed8c9
Irvan Castro <castroirvan@gmail.com>
2023-11-08 16:34:30 +0800

    -Implement read property of Book correctly -Create displayBook function

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = Boolean(read);
+    this.read = Boolean(read) ? "Read" : "Not read";
 };
 
-const sampleBook = new Book("Atomic Habits", "James Clear", 310, true);
+const sampleBook = new Book("Rich Dad Poor Dad", "Robert Kiyasaki", 360, false);
+
+const sampleBook2 = new Book("The Subtle Art", "Mark Manson", 280, false)
 
 console.log(sampleBook);
 
+library.push(sampleBook2);
 
 function addBookToLibrary(book) {
     library.push(book);
@@ -24,5 +27,26 @@ console.log(library);
 
 
 function displayBook() {
-    
+    const bookCtn = document.querySelector(".book-ctn");
+
+    library.forEach(item => {
+        console.log("Book: ", item.title);
+
+        bookCtn.insertAdjacentHTML("afterbegin", `
+        <div class="book">
+            <div class="book-description">
+                <h1 class="title">${item.title}</h1>
+                <h3 class="author">${item.author}</h3>
+                <p>Pages: ${item.pages}</p>
+            </div>
+
+            <div class="button-ctn">
+            <button type="button" class="read-btn">${item.read}</button>
+                <button type="button" class="dlt-btn">Delete</button>
+            </div>
+        </div>
+        `);
+    });
 };
+
+displayBook();
