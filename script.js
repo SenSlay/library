2241d1db531ee489f41de33546949507210f4d91
Irvan Castro <castroirvan@gmail.com>
2023-11-18 20:56:25 +0800

    add(id): Add id property to the book object
    -Implemented a function to generate a unique ID everytime a new
     book is submitted.
    -This will allow the code to precisely refer to the item in the
     library array which the user wants to delete.

--- script.js
+++ script.js
@@ -2,16 +2,13 @@
 let library = [];
 
 // Constructor function for book
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, id) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = Boolean(read) ? "Read" : "Not read";
+    this.id = id;
 };
-
-// Sample book
-const sampleBook = new Book("Rich Dad Poor Dad", "Robert Kiyasaki", 360, false);
-addBookToLibrary(sampleBook);
 
 function addBookToLibrary(book) {
     library.push(book);
@@ -33,6 +30,7 @@ function displayBooks() {
                 <h1 class="title">${item.title}</h1>
                 <h3 class="author">${item.author}</h3>
                 <p>Pages: ${item.pages}</p>
+                <div id="book-id" style="display: none;">${item.id}</div>
             </div>
 
             <div class="button-ctn">
@@ -67,7 +65,9 @@ form.addEventListener("submit", function(event) {
     const pages = document.getElementById("pages").value;
     const isRead = document.getElementById("is-read").value;
 
-    const book = new Book(title, author, pages, isRead);
+    const bookId = generateUniqueId();
+
+    const book = new Book(title, author, pages, isRead, bookId);
 
     addBookToLibrary(book);
     saveData();
@@ -78,14 +78,46 @@ form.addEventListener("submit", function(event) {
     this.reset();
 });
 
+const bookCtn = document.querySelector(".book-ctn");
+
+bookCtn.addEventListener("click", (event) => {
+    if (event.target.classList.contains('dlt-btn')) {
+        if (confirm('Confirm to delete')) {
+            // Delete the book element
+            event.target.closest('.book').remove();
+
+            saveData();
+            console.log(library);
+            console.log(event.target);
+        }
+    }
+});
+
+// Retrieve the idCounter value from localStorage, or set it to 0 if it doesn't exist
+let idCounter = localStorage.getItem('idCounter') ? parseInt(localStorage.getItem('idCounter')) : 1;
+
+function generateUniqueId() {
+    const uniqueId = idCounter;
+    idCounter++;
+
+    // Save updated idCounter so book ids are consistent
+    localStorage.setItem('idCounter', idCounter);
+    return uniqueId;
+};
+
 // Save stringified library array
 function saveData() {
     localStorage.setItem("data", JSON.stringify(library));
-}
+};
 
 // Retrieve saved user data
 function retrieveData() {
     return JSON.parse(localStorage.getItem("data"));
-}
+};
 
+// Sample book
+const sampleBook = new Book("Rich Dad Poor Dad", "Robert Kiyasaki", 360, false, 0);
+
+addBookToLibrary(sampleBook);
+saveData();
 displayBooks();
